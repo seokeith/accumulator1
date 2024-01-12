d994ab6f03ce5c4667e70a4e23baf0292b2e9c95
seokeith <141214829+seokeith@users.noreply.github.com>
2024-01-12 09:29:22 +0000

    Update accum.py

--- accum.py
+++ accum.py
@@ -19,7 +19,7 @@ def calculate_payout(unit_stake, odds, each_way, each_way_fraction):
 st.title("Accumulator Bet Payout Calculator")
 
 # User inputs
-unit_stake = st.number_input("Enter your unit stake", min_value=0.0, value=10.0)
+unit_stake = st.number_input("Enter your unit stake: note this will be doubled in the calculation when Each Way is selected.", min_value=0.0, value=10.0)
 number_of_bets = st.slider("Select number of bets", 1, 10, 3)
 each_way = st.checkbox("Each Way Bet")
 each_way_fraction = st.selectbox("Select Each Way Fraction", ["1/1", "1/2", "1/3", "1/4", "1/5", "1/6", "1/7", "1/8"], index=3) if each_way else "1/1"
